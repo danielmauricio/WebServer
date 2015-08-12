e8fd1ce06e797651dadb533b223c687e0576d19e
Danielon <danielmauriciobm@hotmail.com>
2015-08-12 12:14:15 -0600

    Adding GET-POST-DELETE methods

--- main.c
+++ main.c
@@ -40,34 +40,72 @@ void* web(void* arguments) {
     int fd =args->fd;
     int id = args->id;
     int file_fd, buflen;
+    int file = 5;
     long i, ret, len;
+    long paramPost=0;
+
     char * fstr;
     static char buffer[BUFSIZE+1];
+    static char postVariables[BUFSIZE+1];
 
+    //Recibe los parametros de entrada del Web Server
     ret =read(fd,buffer,BUFSIZE);
+
+    if(!strncmp(buffer,"POST ",5)){
+        //Si el metodo es POST entonces recibe las variables que desea ingresar
+        paramPost = read(fd,postVariables,BUFSIZE);
+    }
+
     if(ret == 0 || ret == -1) {
     }
-    if(ret > 0 && ret < BUFSIZE)
+
+
+    if((ret > 0 && ret < BUFSIZE) || (paramPost>0 && paramPost<BUFSIZE)){
         buffer[ret]=0;
-    else buffer[0]=0;
+        postVariables[paramPost]=0;
+    }
+    else {
+        buffer[0]=0;
+        postVariables[0]=0;
+    }
+
 
     for(i=0;i<ret;i++)
         if(buffer[i] == '\r' || buffer[i] == '\n')
             buffer[i]='*';
-    if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) {
+
+
+    //Si el metodo HTTP no es valido, envia un mensaje.
+    if(!strncmp(buffer,"HEAD ",5) && !strncmp(buffer,"head ",5) ) {
         (void)write(fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this webserver.\n</body></html>\n",271);
         close(fd);
         busyThreads[id] = 0;
         printf("\nSe libero el hilo: %d\n",id+1);
         return NULL;
     }
-    for(i=4;i<BUFSIZE;i++) {
+
+
+    if(!strncmp(buffer,"GET ",4)){
+        file=5;                                     //El "file" varía
+    }                                               //dependiendo del metodo que sea, debido a
+    else if(!strncmp(buffer,"POST ",5)){            //el largo de la palabra
+        file = 6;
+    }
+    else if(!strncmp(buffer,"DELETE ",7)){
+        file = 8;
+    }
+
+
+    for(i=file-1;i<BUFSIZE;i++) {
         if(buffer[i] == ' ') {
             buffer[i] = 0;
             break;
         }
     }
+
+    //Largo del buffer
     buflen=strlen(buffer);
+
     if(strstr(buffer,"/cgi-bin") != NULL || strstr(buffer,".cgi")){
         char * params;
         int paramsIndex =0;
@@ -130,33 +168,50 @@ void* web(void* arguments) {
     for(i=0;extensions[i].ext != 0;i++) {
         len = strlen(extensions[i].ext);
         if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
-                fstr = extensions[i].filetype;
-                break;
+            fstr = extensions[i].filetype;
+            break;
         }
     }
 
 
-        if ((file_fd = open(&buffer[5], O_RDONLY)) == -1) {
-            (void) write(fd,
-                         "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",
-                         224);
-            close(fd);
-            busyThreads[id] = 0;
-            printf("\nSe libero el hilo: %d\n", id + 1);
-            return NULL;
-        }
+    if ((file_fd = open(&buffer[file], O_RDONLY)) == -1) {
+        (void) write(fd,
+                     "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",
+                     224);
+        close(fd);
+        busyThreads[id] = 0;
+        printf("\nSe libero el hilo: %d\n", id + 1);
+        return NULL;
+    }
 
 
-        len = (long) lseek(file_fd, (off_t) 0, SEEK_END);
-        (void) lseek(file_fd, (off_t) 0, SEEK_SET);
-        (void) sprintf(buffer,
-                       "HTTP/1.1 200 OK\nServer: thread-webserver\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n",
-                       len, fstr);
-        (void) write(fd, buffer, strlen(buffer));
+    if(!strncmp(buffer,"GET ",4)){
+        len = (long)lseek(file_fd, (off_t)0, SEEK_END);
+        (void)lseek(file_fd, (off_t)0, SEEK_SET);
+        (void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: thread-webserver\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", len, fstr);
+        (void)write(fd,buffer,strlen(buffer));
+    }
 
-        while ((ret = read(file_fd, buffer, BUFSIZE)) > 0) {
-            (void) write(fd, buffer, ret);
-        }
+    else if(!strncmp(buffer,"POST ",5)){
+        (void)lseek(file_fd, (off_t)0, SEEK_SET);
+        char postdata[buflen];
+        strcpy(postdata, postVariables);  /* Change param and value here */
+        printf("%s",postVariables);
+        int length;
+        length = (int) strlen(postdata);
+        sprintf(buffer,"POST %s HTTP1.1\r\n Host: localhost \r\n Server: thread-webserver\r\nAccept: */*\r\nAccept-Language: en-us\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept-Encoding: gzip,deflate\r\nContent-Length: %d\r\nPragma: no-cache\r\nConnection: keep-alive\r\n\r\n%s",buffer,length,postdata);
+        (void)write(fd,buffer,strlen(buffer));
+    }
+
+    else if(!strncmp(buffer,"DELETE ",7)){
+        remove(buffer);
+        sprintf(buffer,"HTTP/1.1 200 OK\nServer: thread-webserver\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", len, fstr);
+        (void)write(fd,buffer,strlen(buffer));
+    }
+
+    while ((ret = read(file_fd, buffer, BUFSIZE))> 0 ) {
+        (void)write(fd,buffer,ret);
+    }
 
 
     close(fd);
